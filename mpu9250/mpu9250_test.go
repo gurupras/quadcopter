5823b4c0c87eb217abe15baed5713ec8170c38ed
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2022-03-21 08:23:40 -0400

    fixup

--- mpu9250/mpu9250_test.go
+++ mpu9250/mpu9250_test.go
@@ -3,10 +3,20 @@ package mpu9250
 import (
 	"testing"
 	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMPU9250Basic(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+
+	require := require.New(t)
+
 	dev := New(1)
+
+	err := dev.Init()
+	require.Nil(err)
 
 	for i := 0; i < 100; i++ {
 		dev.ReadSample()
